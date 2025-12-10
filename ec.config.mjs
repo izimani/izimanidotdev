c53eb41d9fb3a79202663396c8891c945434b32c
Attila Bagossy <bagossyattila@outlook.com>
2025-12-10 16:53:48 +0100

    feat: code blocks now respect the dark mode setting

--- ec.config.mjs
+++ ec.config.mjs
@@ -3,7 +3,7 @@ import { pluginCollapsibleSections } from "@expressive-code/plugin-collapsible-s
 import { pluginLineNumbers } from "@expressive-code/plugin-line-numbers";
 
 export default defineEcConfig({
-  themes: ["github-dark"],
+  themes: ["github-light", "github-dark"],
 
   defaultProps: {
     wrap: true,
