c052dc63079286534175a6d5676bcadb324fd4ed
Attila Bagossy <bagossyattila@outlook.com>
2025-12-22 20:55:32 +0100

    feat: site setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,18 @@ import playformCompress from "@playform/compress";
 import markdoc from "@astrojs/markdoc";
 import expressiveCode from "astro-expressive-code";
 
+function getSiteUrl() {
+  const presetSite = process.env.SITE;
+  let url = presetSite;
+  if (!presetSite) {
+    url = "http://localhost:4321";
+  }
+
+  console.log("Site URL:", url);
+
+  return url;
+}
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [
@@ -26,6 +38,6 @@ export default defineConfig({
     plugins: [],
   },
 
-  site: "http://localhost:4321",
+  site: getSiteUrl(),
   output: "static",
 });
